0bf7f4ad2775b9ff3be49f181cb8df95f801f134
Omar Alkhowaiter <OmarMAlkhowaiter@gmail.com>
2023-07-06 07:19:41 +0300

    Revealing animation

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-/*
-const galleryItems = document.querySelectorAll('.gallery-item');
-
-function revealOnScroll() {
-  const windowHeight = window.innerHeight;
-  galleryItems.forEach(item => {
-    const itemTop = item.getBoundingClientRect().top;
-    if (itemTop < windowHeight) {
-      item.classList.add('reveal');
-    }
-  });
-}
-
-window.addEventListener('scroll', revealOnScroll);
-
-const navMenu = document.getElementById('nav-menu'),
-  toggleMenu = document.getElementById('toggle-menu'),
-  closeMenu = document.getElementById('close-menu')
-
-toggleMenu.addEventListener('click', () => {
-  navMenu.classList.toggle('show')
-})
-closeMenu.addEventListener('click', () => {
-  navMenu.classList.remove('show')
-})
-*/ 
 const observer = new IntersectionObserver(entries => {
   entries.forEach((entry) => {
     console.log(entry);
