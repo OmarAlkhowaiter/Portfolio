ff6a1ec98bb037c5f9f47b8b075ed1db11e39833
Omar Alkhowaiter <OmarMAlkhowaiter@gmail.com>
2023-07-06 07:18:20 +0300

    Revealing animation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const galleryItems = document.querySelectorAll('.gallery-item');
 
 function revealOnScroll() {
@@ -22,3 +23,21 @@ toggleMenu.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
   navMenu.classList.remove('show')
 })
+*/ 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+if (hiddenElements.length > 0) {
+  hiddenElements.forEach((el) => observer.observe(el));
+} else {
+  console.error('No elements found with class "hidden"');
+}
